1fbfeb335a308554ade9f7b4d503c3a5e904130a
swashik-007 <mohanty.swastik4@gmail.com>
2024-04-06 12:47:42 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+      Welcome to my React App
+
+      </h1>
+    </div>
+  );
+}
+
+export default App;
